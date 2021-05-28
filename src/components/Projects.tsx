d1a2d46c28ce80816a98a50b4a8d5dd2255130c1
Joseph Hall <josephdahall@hotmail.co.nz>
2021-05-28 14:34:22 +1200

    Added email survey website project to the projects page.

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,6 +2,7 @@ import { Container, Col, Row } from "react-bootstrap";
 import pidphoto from "../content/images/PIDController_Code.png";
 import policeloginphoto from "../content/images/PolicePhotoTagging.png";
 import mapscreenshot from "../content/images/Map.png";
+import emailyscreenshot from "../content/images/EmailyScreenshot.png";
 
 const Projects = () => (
 	<div>
@@ -54,6 +55,28 @@ const Projects = () => (
 				</Col>
 				<Col lg={{ span: 1, order: 1 }} />
 			</Row>
+			<Row className="content-row">
+				<Col lg={12} sm={12}>
+					<a href="https://github.com/JoeHall97/FullStackCourseApp">
+						<h1 className="display-4 text-center" style={{ paddingBottom: "50px" }}>
+							Email Survey Website
+						</h1>
+					</a>
+				</Col>
+				<Col lg={1} />
+				<Col lg={3} sm={12}>
+					<img className="img-fluid" src={emailyscreenshot} alt="Login screen" />
+				</Col>
+				<Col lg={7} sm={12}>
+					<p className="mb-0">
+					As part of an online course I, built a full stack website that utilises, NodeJS, ReactJS and MongoDB. 
+					The website allows users to create and send out email surveys, and view the responses to the survey(s). 
+					I am currently working on redesigning the front-end, implementing some new features, and converting the code base over to typescript.
+					The site is hosted over on <a href='https://emaily-test-project-joehall.herokuapp.com/'>Heroku.</a>
+					</p>
+				</Col>
+				<Col lg={1} />
+			</Row>
 			<Row className="content-row-bottom">
 				<Col lg={12} sm={12}>
 					<a href="https://github.com/JoeHall97/PathFinder">
@@ -62,17 +85,17 @@ const Projects = () => (
 						</h1>
 					</a>
 				</Col>
-				<Col lg={1} />
-				<Col lg={3} sm={12}>
+				<Col lg={{ span: 1, order: 12 }} />
+				<Col lg={{ span: 3, order: 11 }} sm={12}>
 					<img className="img-fluid" src={mapscreenshot} alt="Screenshot of a map file" />
 				</Col>
-				<Col lg={7} sm={12}>
+				<Col lg={{ span: 7, order: 2 }} sm={12}>
 					<p className="mb-0">
 						A python program that I've built to find the shortest path between two points on a map file. The program can
 						use depth-first, breadth-first, best-first, and A* algorithms to find this path.
 					</p>
 				</Col>
-				<Col lg={1} />
+				<Col lg={{ span: 1, order: 1 }} />
 			</Row>
 		</Container>
 	</div>
